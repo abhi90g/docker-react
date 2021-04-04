c123b9b844e74501700e798579780151a9051af7
Abhinav Gupta <abhinavgupta@Abhinavs-MBP.lan>
2021-04-04 12:46:21 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hellooooooo!!!!
+          this is a new text on app page
         </p>
         <a
           className="App-link"
